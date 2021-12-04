f7219e4fb85ea9e846dd52441e9c680ae434fec2
CiGarCiGa <ci.garcia81@uniandes.edu.co>
2021-12-04 10:44:49 -0500

    Update services.py

--- flaskr/services/services.py
+++ flaskr/services/services.py
@@ -43,7 +43,8 @@ def convert_audio(id: int):
             logger.info("localSourceFile: " + localSourceFile)
             logger.info("s3SourceFile: " + s3SourceFile)
             
-            util.downloadFile(localSourceFile,s3SourceFile)
+            s3file=util.downloadFileS3(s3SourceFile)
+            logger.info("Tipo Archivo" + str(type(s3file)))
             logger.info("Archivo descargado: " + s3SourceFile)
             localProcessedFile = os.path.join(TEMP_PROCESSED_FOLDER, task.uploaded_file + "." + task.processed_format.name.lower())
             logger.info("Nuevo archivo local: " + localProcessedFile)
